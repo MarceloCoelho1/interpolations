535c6e56b6ef9bc55b516b7f9c88468d0c5677fd
Marcelo <mhbcoelho99@gmail.com>
2023-11-23 10:31:16 -0300

    change to interpolate frames

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -1,14 +1,23 @@
-def linear_interpolation(x, x1, y1, x2, y2):
+import cv2
+
+def linear_interpolation(x, x1, frame1_path, x2, frame2_path, output_path):
+    # Carrega os frames
+    frame1 = cv2.imread(frame1_path)
+    frame2 = cv2.imread(frame2_path)
+
     if x1 == x2:
         raise ValueError("Os pontos x1 e x2 não podem ser iguais para a interpolação linear.")
 
-    y = y1 + (x - x1) * (y2 - y1) / (x2 - x1)
-    return y
+    # Realiza a interpolação linear
+    interpolated_frame = cv2.addWeighted(frame1, 1 - (x - x1) / (x2 - x1), frame2, (x - x1) / (x2 - x1), 0)
 
+    # Salva o frame interpolado
+    cv2.imwrite(output_path, interpolated_frame)
 
-x1, y1 = 2, 3
-x2, y2 = 5, 8
+# Exemplo de uso:
+x1, frame1_path = 2, "caminho/do/seu/output/folder/frame_0001.png"
+x2, frame2_path = 5, "caminho/do/seu/output/folder/frame_0005.png"
 x_interpolate = 3
-result = linear_interpolation(x_interpolate, x1, y1, x2, y2)
-print(f"A interpolação linear de {x_interpolate} entre ({x1}, {y1}) e ({x2}, {y2}) é {result}")
+output_path = "caminho/do/seu/output/folder/interpolated_frame.png"
 
+linear_interpolation(x_interpolate, x1, frame1_path, x2, frame2_path, output_path)
