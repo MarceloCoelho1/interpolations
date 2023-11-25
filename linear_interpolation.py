71c1a4e9b50e84bc997b3321a3580cbe12447c58
Marcelo <mhbcoelho99@gmail.com>
2023-11-25 12:29:35 -0300

    fix some bugs

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -1,6 +1,7 @@
 import cv2
+import numpy as np
 
-def linear_interpolation(x, x1, frame1_path, x2, frame2_path, output_path):
+def linear_interpolation(x, x1, frame1_path, x2, frame2_path):
     # Carrega os frames
     frame1 = cv2.imread(frame1_path)
     frame2 = cv2.imread(frame2_path)
@@ -9,15 +10,13 @@ def linear_interpolation(x, x1, frame1_path, x2, frame2_path, output_path):
         raise ValueError("Os pontos x1 e x2 não podem ser iguais para a interpolação linear.")
 
     # Realiza a interpolação linear
-    interpolated_frame = cv2.addWeighted(frame1, 1 - (x - x1) / (x2 - x1), frame2, (x - x1) / (x2 - x1), 0)
-
-    # Salva o frame interpolado
-    cv2.imwrite(output_path, interpolated_frame)
+    interpolated_frame = frame1 + (x - x1) * (frame2 - frame1) / (x2 - x1)
+    return interpolated_frame.astype(np.uint8)
 
 # Exemplo de uso:
 x1, frame1_path = 2, "/home/celin/Desktop/interpolation/frames/frame_0002.png"
 x2, frame2_path = 5, "/home/celin/Desktop/interpolation/frames/frame_0005.png"
 x_interpolate = 3
-output_path = "/home/celin/Desktop/interpolation/frames/frame_0003_interpolate.png"
 
-linear_interpolation(x_interpolate, x1, frame1_path, x2, frame2_path, output_path)
+result = linear_interpolation(x_interpolate, x1, frame1_path, x2, frame2_path)
+cv2.imwrite('/home/celin/Desktop/interpolation/frames/frame_0003_interpolate.png', result)
