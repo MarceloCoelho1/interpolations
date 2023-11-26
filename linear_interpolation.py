a7db7ab7eb66d2ca331a6d54a77e12d4ab9fac88
Marcelo <mhbcoelho99@gmail.com>
2023-11-26 14:35:51 -0300

    adding truncation error

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -11,12 +11,32 @@ def linear_interpolation(x, x1, frame1_path, x2, frame2_path):
 
     # Realiza a interpolação linear
     interpolated_frame = frame1 + (x - x1) * (frame2 - frame1) / (x2 - x1)
+
+
     return interpolated_frame.astype(np.uint8)
+
+def calculate_second_derivative(frame, x):
+    # Aproximação da segunda derivada usando diferença central
+    f_xx = frame[x + 1] - 2 * frame[x] + frame[x - 1]
+    return f_xx
+
+def calculate_truncation_error(x, x1, x2, frame1, frame2):
+    # Aproximação da segunda derivada das intensidades de pixel
+    f_double_prime = calculate_second_derivative(frame1, x1) / 2 + calculate_second_derivative(frame2, x2) / 2
+
+    # Calcule o erro de truncamento
+    error = ((x - x1) * (x - x2) / 2) * f_double_prime
+    return error
 
 # Exemplo de uso:
 x1, frame1_path = 2, "/home/celin/Desktop/interpolation/frames/frame_0002.png"
 x2, frame2_path = 5, "/home/celin/Desktop/interpolation/frames/frame_0005.png"
 x_interpolate = 3
+frame1 = cv2.imread(frame1_path)
+frame2 = cv2.imread(frame2_path)
 
 result = linear_interpolation(x_interpolate, x1, frame1_path, x2, frame2_path)
-cv2.imwrite('/home/celin/Desktop/interpolation/frames/frame_0003_interpolate.png', result)
+# cv2.imwrite('/home/celin/Desktop/interpolation/frames/frame_0003_interpolate.png', result)
+
+truncation_error = calculate_truncation_error(x_interpolate, x1, x2, frame1, frame2)
+print(truncation_error)
